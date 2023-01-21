9bb7f8ee0908b4bed5c5a9a5d92cf5788144fc2d
Sagar Subedi <sagar@example.com>
2023-01-21 11:32:59 -0600

    initial build

---
+++ src/main/java/com/example/springmaster/SpringmasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmaster/SpringmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
